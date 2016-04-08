6e5ed6162569e2379ede37170261dc5da72ab22c
dmnm <dm-nm@nm.ru>
2016-04-08 12:42:34 +0300

    hotels: some updates

--- src/main/java/org/example/rest/entity/Hotel.java
+++ src/main/java/org/example/rest/entity/Hotel.java
@@ -3,12 +3,16 @@ package org.example.rest.entity;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.constraints.NotNull;
+
 public class Hotel extends Entity {
     private static final long serialVersionUID = 1730048616163340749L;
 
+    @NotNull
     public String name;
-    public transient List<Room> rooms = new ArrayList<>();
-    public HotelType type;
+    // @JsonIgnore
+    public List<Room> rooms = new ArrayList<>();
+    public HotelType type = HotelType.SIMPLE;
     public boolean hasPool;
     public boolean hasWaterslides;
     public boolean hasTennisCourt;
